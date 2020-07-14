a684a0755568767047f89efa71aac6402aea03c4
Ude Samuel <59575776+JesusKreist@users.noreply.github.com>
2020-07-14 21:30:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,55 @@
-//placeholder
-//more placeholder
+(function() {
+    const displayScreen = document.querySelector(".screen");
+    const btns = document.getElementsByClassName("btn btn-grey");
+    const myOperators = document.getElementsByClassName("btn btn-yellow");
+    const clearBtn = document.querySelector(".btn-clear");
+    const equalBtn = document.querySelector(".btn-equal");
+    const answerButton = document.querySelector(".btn-ans");
+
+    let computeValues = "";
+    let lastAnswer = null;
+
+
+    const clearScreen = () => {
+        displayScreen.value = "";
+        computeValues = "";
+    };
+
+
+    const compute = (event) => {
+        let stringNumber = String(event.target.dataset.num);
+        computeValues += stringNumber;
+        displayScreen.value = computeValues;
+    };
+
+    const calculate = () => {
+        try {
+            let result = math.evaluate(computeValues);
+            computeValues = "";
+            displayScreen.value = result;
+            lastAnswer = result;
+
+        } catch (error) {
+            displayScreen.value = "Please Check Your Expression!";
+        };
+    };
+
+    const lastResult = () => {
+        computeValues += lastAnswer;
+        displayScreen.value = computeValues;
+    };
+
+    // wire up all the buttons
+    clearBtn.addEventListener("click", clearScreen);
+    equalBtn.addEventListener("click", calculate);
+    answerButton.addEventListener("click", lastResult);
+
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", compute);
+    };
+
+    for (let i = 0; i < myOperators.length; i++) {
+        myOperators[i].addEventListener("click", compute);
+    }; 
+})();
